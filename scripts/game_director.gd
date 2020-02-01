b80dce88f68490eed9866ca0893254daf738d7f6
Joschka Lingemann <j.lingemann@gmail.com>
2020-02-01 10:24:44 +0100

    Added initial scenario and game director

---
+++ scripts/game_director.gd
@@ -0,0 +1,25 @@
+extends Node
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+var _scenario = {}
+var _stage = {}
+var stage_cntr = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var file = File.new()
+	file.open("res://scenarios/crash_001.json", file.READ)
+	_scenario.parse_json(file.get_as_text())
+	_set_next_stage()
+	
+func _set_next_stage():
+	_stage = _scenario["stages"][stage_cntr]
+	
+func resolve_input(input_array : Array):
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
