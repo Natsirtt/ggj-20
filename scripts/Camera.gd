362745f9999152c28d85d5c93c892d476ba80d8b
Tristan Louet <tristan.louet.dev@gmail.com>
2020-02-01 16:20:53 +0100

    Camera stages

--- scripts/Camera.gd
+++ scripts/Camera.gd
@@ -6,11 +6,20 @@ export var shake_strength = 1.0
 export (Vector3) var up_stage_rotation = Vector3.ZERO
 export (Vector3) var down_stage_rotation = Vector3.ZERO
 
+onready var stage_to_rotation = {
+	Stage.UP: up_stage_rotation,
+	Stage.DOWN: down_stage_rotation
+}
+
 export (Stage) var stage = Stage.UP
 
 func set_stage(new_stage):
 	if new_stage != stage:
 		stage = new_stage
+		set_rotation(stage_to_rotation[stage])
 
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("camera_down"):
+		set_stage(Stage.DOWN)
+	elif Input.is_action_just_pressed("camera_up"):
+		set_stage(Stage.UP)
