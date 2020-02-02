37163e47f6344464154b00e316aa7ffe92b150e1
jlingema <j.lingemann@gmail.com>
2020-02-02 13:01:22 +0100

    Added a nicer main menu

--- game_scenes/props/label_3d.gd
+++ game_scenes/props/label_3d.gd
@@ -1,8 +1,13 @@
 tool
 extends Spatial
 
-export(String) var label_text = "default" setget set_label_text, get_label_text
+export(String) var label_text = "default" setget set_label_text
+export(Color) var label_color = Color.black setget set_label_color
 
+func set_label_color(color):
+	label_color = color
+	$Viewport/Control/Label.add_color_override("font_color", label_color)
+	
 func set_label_text(text):
 	label_text = text
 	$Viewport/Control/Label.text = text
@@ -10,9 +15,6 @@ func set_label_text(text):
 	$Viewport/Control/Label.margin_top = 0
 	$Viewport/Control/Label.margin_left = 0
 	$Viewport/Control/Label.margin_right = 0
-	
-func get_label_text():
-	return label_text
 	
 func _ready():
 	pass # Replace with function body.
